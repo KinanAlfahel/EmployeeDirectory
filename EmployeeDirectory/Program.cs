51979b6020726d54ae42911c6ca6c96e95261de4
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-06-16 13:50:27 +0200

    Add project files.

---
+++ EmployeeDirectory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot(12 , "Kalle");
+            //var name = robot.GetName();
+            robot.Height = 25;
+       
+        }
+    }
+}
---
+++ EmployeeDirectory/Robot.cs
@@ -0,0 +1,37 @@
+﻿namespace EmployeeDirectory
+{
+    public class Robot
+    {
+        // private string name;
+
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+
+        public string Name { get; set; }
+     
+
+
+        public Robot(int height, string name)
+        {
+            Name = name;
+        }
+
+
+        //public string GetName()
+        //{
+        //    return name;
+        //}
+
+        //public void SetName(string newName)
+        //{
+        //    //Validate
+        //    name = newName;
+        //}
+    }
+}
